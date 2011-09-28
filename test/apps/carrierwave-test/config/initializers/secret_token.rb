8ff5c6f84c3e4286f16bf91d5545f669f6939f94
Pablo Brasero <pablobm@gmail.com>
2011-09-28 14:20:28 +0100

    Example Rails 3.1 app, to test CarrierWave compatibility

---
+++ test/apps/carrierwave-test/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CarrierwaveTest::Application.config.secret_token = '336fb509c02c966b1a91ae6cc604d5ce9ea9e16a6d23f8a3c0b370f9ac21992dd6fbea72dde1a1ccb86eb9e4a9f6a3b1d25c5ba3a55e0be89148e9c036b70e81'

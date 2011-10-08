4d071db5223bc06b49ee094245837083ea5efd7e
Pablo Brasero Moreno <pablobm@gmail.com>
2011-10-08 01:34:33 +0100

    Version 1.0.8

--- lib/rack/raw_upload.rb
+++ lib/rack/raw_upload.rb
@@ -3,7 +3,7 @@ require 'tmpdir' # Needed in 1.8.7 to access Dir::tmpdir
 module Rack
   class RawUpload
 
-    VERSION = '1.0.7'
+    VERSION = '1.0.8'
 
     def initialize(app, opts = {})
       @app = app
